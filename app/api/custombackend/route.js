1db3acfc2a7944f642c30ac02a171afe80d4cc51
Asadullah246 <asadm2258@gmail.com>
2024-01-26 15:06:23 +0600

    removed custom bakcend code

--- app/api/custombackend/route.js
+++ app/api/custombackend/route.js
@@ -1,35 +1,35 @@
-import { NextResponse } from "next/server";
-require('dotenv').config();
-const mysql = require('mysql2/promise'); // Using promise-based version
+// import { NextResponse } from "next/server";
+// require('dotenv').config();
+// const mysql = require('mysql2/promise'); // Using promise-based version
 
-const pool = mysql.createPool(process.env.DATABASE_SERVER);
-console.log('Connected to PlanetScale!');
+// const pool = mysql.createPool(process.env.DATABASE_SERVER);
+// console.log('Connected to PlanetScale!');
 
-export async function GET(request) {
-  console.log("calling this get method");
+// export async function GET(request) {
+//   console.log("calling this get method");
 
-  const data = [ 
-    {
-      name: "dkjfd",
-      use: "djfd",
-    },
-    {
-      name: "dkjfdfd",
-      use: "ddffjfd",
-    },
-  ];
+//   const data = [
+//     {
+//       name: "dkjfd",
+//       use: "djfd",
+//     },
+//     {
+//       name: "dkjfdfd",
+//       use: "ddffjfd",
+//     },
+//   ];
 
-  try {
-    const connection = await pool.getConnection();
-    const res = await connection.query('SELECT * FROM Category');
-    connection.release();
-    console.log("re", res)
-    // return NextResponse.json(data);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ message: error }, { status: 500 });
-  }
-}
+//   try {
+//     const connection = await pool.getConnection();
+//     const res = await connection.query('SELECT * FROM Category');
+//     connection.release();
+//     console.log("re", res)
+//     // return NextResponse.json(data);
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ message: error }, { status: 500 });
+//   }
+// }
 
 // import { PrismaClient } from "@prisma/client";
 
@@ -48,14 +48,14 @@ export async function GET(request) {
 // }
 
 
-export function POST(request) {
-  // Your POST logic here
-}
+// export function POST(request) {
+//   // Your POST logic here
+// }
 
-export function PUT(request) {
-  // Your PUT logic here
-}
+// export function PUT(request) {
+//   // Your PUT logic here
+// }
 
-export function DELETE(request) {
-  // Your DELETE logic here
-}
+// export function DELETE(request) {  
+//   // Your DELETE logic here
+// }
